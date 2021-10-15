3e3a4dad9d44c428b6f3460dc601ad19c2b9cf9b
RAMENSKII <RA>
2021-10-15 18:33:15 +0700

    Initial Commit

---
+++ PyaterochkaExampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PyaterochkaExampleProject
+//
+//  Created by Kirill on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
